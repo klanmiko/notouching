06d1e72c419e808926e055904512a7c98c8f9089
klanmiko <eternity01033@gmail.com>
2016-08-29 22:34:36 -0700

    final project additions

---
+++ typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/body-parser/index.d.ts" />
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/passport/index.d.ts" />
+/// <reference path="globals/request/index.d.ts" />
+/// <reference path="globals/socket.io/index.d.ts" />
